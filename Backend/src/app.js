840edfde3b28688393d38989b235e5aeb9b371c1
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-02-25 01:18:22 +0530

    add util

---
+++ Backend/src/app.js
@@ -0,0 +1,17 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from 'cors'
+import cookieparser from "cookie-parser"
+dotenv.config()
+
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORGIN
+}))
+app.use(express.json({limit : "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(cookieparser)
+
+export { app }
