0cabecc876efba5a09173cfd47cbebd9614aeac4
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-03-02 22:41:18 +0530

    authentication complete

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,9 +7,8 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORGIN,
-    credentials: true,
-}))
+    origin: 'http://localhost:5173'
+  }));
 app.use(express.json({limit : "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(cookieparser())
