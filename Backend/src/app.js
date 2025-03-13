51594a85bd1fc6d6ca0a6675898997c616962726
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-03-13 23:41:32 +0530

    Fix missing ProductItem file

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,9 +6,7 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors({
-    origin: 'http://localhost:5173'
-  }));
+app.use(cors());
 app.use(express.json({limit : "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(cookieparser())
