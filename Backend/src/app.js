40bfd25e8d205838fb0a0e2c82e24717cc4785c3
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-04-05 13:47:46 +0530

    edit app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,23 +1,59 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from 'cors'
-import cookieparser from "cookie-parser"
-dotenv.config()
+// import express from "express"
+// import dotenv from "dotenv"
+// import cors from 'cors'
+// import cookieparser from "cookie-parser"
+// dotenv.config()
 
-const app = express()
+// const app = express()
+
+// app.use(cors({ origin: "*" }));
+// app.use(express.json({limit : "16kb"}))
+// app.use(express.urlencoded({extended: true, limit: "16kb"}))
+// app.use(cookieparser())
+
+// //routes import 
+// import userRouter from "./routes/user.routes.js"
+
+// app.get("/", (req, res) => {
+//   res.send("API is working on Vercel!");
+// });
+
+// app.use("/api/user", userRouter)
+
+// export { app }
+
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieparser from "cookie-parser";
+import connectDB from "./db/index.js";
+dotenv.config();
+
+const app = express();
 
 app.use(cors({ origin: "*" }));
-app.use(express.json({limit : "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(cookieparser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieparser());
 
-//routes import 
-import userRouter from "./routes/user.routes.js"
+app.use(async (req, res, next) => {
+  connectDB()
+    .then(() => {
+      next();
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+//routes import
+
+import userRouter from "./routes/user.routes.js";
 
 app.get("/", (req, res) => {
   res.send("API is working on Vercel!");
 });
 
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
 
-export { app }
+export { app };
